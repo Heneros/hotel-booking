2d7b1ad3ec0d8717859b9c1172dc7be3ec20edae
Heneros <punk60826@gmail.com>
2021-04-29 22:20:30 +0300

    slider-guests

--- src/js/script.js
+++ src/js/script.js
@@ -10,5 +10,11 @@
         dots: false,
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/arrow-left.svg" alt= ""></button>',
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="img/arrow-right.svg" alt=""></button>',
-    })
+    });
+    $(".slider-guests").slick({
+        dots: false,  
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/left.svg" alt= ""></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="img/right.svg" alt=""></button>',
+    });
+
 }); 
