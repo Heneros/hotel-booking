70e28969ee72029aecdb93bd7e9bb2b7757d3603
Heneros <punk60826@gmail.com>
2021-05-11 00:29:00 +0300

    slider adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,18 @@
         dots: false,
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/arrow-left.svg" alt= ""></button>',
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="img/arrow-right.svg" alt=""></button>',
+        responsive: [
+        
+              {
+                breakpoint: 730,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true,
+                    arrows: false,
+                }
+              }
+        ]
+
     });
     $(".slider-guests").slick({
         dots: false,  
