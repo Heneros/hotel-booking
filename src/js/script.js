7443f6df728e195a160ba45099d30fc9e6033382
Heneros <punk60826@gmail.com>
2021-05-13 00:54:48 +0300

    ...

--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,7 @@
         dots: false,
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/arrow-left.svg" alt= ""></button>',
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="img/arrow-right.svg" alt=""></button>',
-        variableWidth: true,
+     
 
         responsive: [
         
