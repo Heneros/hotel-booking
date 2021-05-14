d35137db49e2238cc0cb8cc55f51d072bcf0a2b6
Heneros <punk60826@gmail.com>
2021-05-14 23:54:56 +0300

    other-offers adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -10,8 +10,6 @@
         dots: false,
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/arrow-left.svg" alt= ""></button>',
         nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="img/arrow-right.svg" alt=""></button>',
-     
-
         responsive: [
         
               {
