6e848fa33452efafb8b0e4f1dde8bd2d11dad2f2
Heneros <punk60826@gmail.com>
2021-06-01 03:52:33 +0300

    modal part1

--- src/js/dev/main.js
+++ src/js/dev/main.js
@@ -1,8 +1,16 @@
 import modal from './modules/modal';
-          
-          
-
+           
+         
+  
 window.addEventListener('DOMContentLoaded', () =>{
    'use strict'; 
    modal();  
-});  
+});    
+    
+
+
+
+var div = document.getElementsByClassName('one')[0];
+div.addEventListener('click', foo, false);
+
+function foo() { alert('Hello!'); }
