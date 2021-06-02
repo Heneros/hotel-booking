bd227fbd9f23134d6773d3be4879d3965bcad436
Heneros <punk60826@gmail.com>
2021-06-02 23:56:37 +0300

    showModal function

--- src/js/dev/modules/modal.js
+++ src/js/dev/modules/modal.js
@@ -58,7 +58,19 @@ const modal = () => {
     function showModalByTime(selector, time){
       setTimeout(function(){ 
         let display;
-      })
+
+        document.querySelectorAll('[data-modal]').forEach(item => {
+          if(getComputedStyle(item).display !== 'non'){
+            display = "block";
+          }
+        });
+        if(!display){
+          document.querySelector(selector).style.display = 'block';
+          document.body.style.overflow = "hidden";
+          let scroll = calcScroll();
+          document.body.style.marginRight = `${scroll}px`
+        }
+      }, time)
     }
 
 
